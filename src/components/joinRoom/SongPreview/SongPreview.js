449fac99047488332b9caa49889f5eee3aaa607e
aromalsanthosh <aromalponmana@gmail.com>
2021-09-30 00:58:21 +0530

    Added fa icons demo

--- src/components/joinRoom/SongPreview/SongPreview.js
+++ src/components/joinRoom/SongPreview/SongPreview.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './SongPreview.css'
 import songIcon from '../../../assets/songicon.png';
 import {Button} from 'react-bootstrap'
-import { BsFillTrashFill } from 'react-icons/fa';
+import { FaTrash,FaPlay, FaUpload } from 'react-icons/fa';
 
 
 function SongPreview(props) {
@@ -12,7 +12,7 @@ function SongPreview(props) {
             <img className="song-icon"src={songIcon} alt="icon-song" />
                 <p>{props.title} Fetched From Link {props.duration}</p>
             </div>
-            <Button className="remove-song-button"><BsFillTrashFill/></Button>
+            <Button className="remove-song-button"><FaTrash/> <FaPlay/> <FaUpload/> </Button>
         </div>
         
     )
