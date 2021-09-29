780584b795487c7f79e41621dad87c640a60f4b5
Akshay T K <akshayashok.tk@gmail.com>
2021-09-30 00:49:22 +0530

    trash icon

--- src/components/joinRoom/SongPreview/SongPreview.js
+++ src/components/joinRoom/SongPreview/SongPreview.js
@@ -2,6 +2,8 @@ import React from 'react'
 import './SongPreview.css'
 import songIcon from '../../../assets/songicon.png';
 import {Button} from 'react-bootstrap'
+import { BsFillTrashFill } from 'react-icons/fa';
+
 
 function SongPreview(props) {
     return (
@@ -10,7 +12,7 @@ function SongPreview(props) {
             <img className="song-icon"src={songIcon} alt="icon-song" />
                 <p>{props.title} Fetched From Link {props.duration}</p>
             </div>
-            <Button className="remove-song-button"></Button>
+            <Button className="remove-song-button"><BsFillTrashFill/></Button>
         </div>
         
     )
